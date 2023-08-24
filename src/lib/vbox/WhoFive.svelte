09f7611d50ac5ebb2fd196c16d2de1b9858c5350
Trey <treycluff@gmail.com>
2023-08-24 18:19:18 -0400

    added basic who5 func

---
+++ src/lib/vbox/WhoFive.svelte
@@ -0,0 +1,22 @@
+<script>
+    
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    let value = 10;
+
+    function updateAnswer(event) {
+        dispatch('whofive', {number: questionNumber, answer: event.target.value});
+    }
+    
+    export let questionNumber;
+    
+
+</script>
+<main>
+    <input type="range" min="0" max="10" bind:value on:change={updateAnswer}>
+</main>
+<style>
+
+</style>
