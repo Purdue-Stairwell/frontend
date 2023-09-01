ed101f0441db178b52597ebc39bacd4ed8896c7c
xM4ddy <59680197+xM4ddy@users.noreply.github.com>
2023-08-31 23:40:52 -0400

    Add Typewriter Effect

--- src/lib/ibox/Narration.svelte
+++ src/lib/ibox/Narration.svelte
@@ -1,31 +1,46 @@
 <script>
-    import { fade, fly } from 'svelte/transition';
-    //import Typewriter from 'typewriter-effect/dist/core';
+    import { fade, fly } from "svelte/transition";
+    import { createEventDispatcher, onMount } from "svelte";
 
     export let scriptPage;
-    
+
+    var i = 0;
+    var txt = scriptPage; /* The text */
+    var speed = 50; /* The speed/duration of the effect in milliseconds */
+    var typed = "";
+
+    function typeWriter() {
+        if (i < txt.length) {
+            typed += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        } //....
+    }
+
+    onMount(() => {
+        typeWriter();
+    });
 </script>
 
 <main>
-    <p transition:fade>{scriptPage}</p>
+    <p>{typed}</p>
 </main>
 
 <style>
     main {
-        
         display: flex;
         justify-content: space-around;
-        background-color: #90D7FF;
+        background-color: #90d7ff;
         align-items: center;
         border-radius: 15px;
         margin: 15px 25px;
         box-shadow: 3px 3px 2px #000015;
     }
-    p{
+    p {
         line-height: 1.5;
         font-size: 14pt;
         height: 150px;
-        background-color: #90D7FF;
+        background-color: #90d7ff;
         padding: 10px;
         border-radius: 15px;
         font-weight: 700;
