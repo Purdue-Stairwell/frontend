4df0cdda889c17961c446b8449ca476a2dfdc2ef
treycluffIRM <treycluff@gmail.com>
2023-09-06 12:53:15 -0400

    updated AudioPlayer

--- src/lib/helpers/AudioPlayer.svelte
+++ src/lib/helpers/AudioPlayer.svelte
@@ -1,10 +1,18 @@
 <script>
-	export let src;
+	export let state;
+
+	const srcs = [
+		"/sound/1Base.mp3",
+		"/sound/1Draw.mp3",
+		"/sound/0906 Who5.mp3",
+		"/sound/0906 2Base.mp3",
+		"/sound/0906 2Draw.mp3",
+	];
 </script>
 
 <audio>
 	<audio autoplay loop>
-		<source {src} type="audio/mpeg" />
+		<source src={srcs[state]} type="audio/mpeg" />
 	</audio>
 </audio>
 
